28cd8af033b28bfafcbbe7efa0a387ddf3c06af8
One Nose <idohaolam@gmail.com>
2024-01-28 20:54:07 +0200

    Add tracing on minus

--- src/Boot.hx
+++ src/Boot.hx
@@ -46,6 +46,7 @@ class Boot extends hxd.App {
             // (-) Toggle normal, slow, and very slow speeds
             if (Key.isPressed(Key.NUMPAD_SUB) || Key.isPressed(Key.QWERTY_MINUS)) {
                 this.speed = if (this.speed == 1) 0.35 else if (this.speed == 0.35) 0.1 else 1;
+                trace('speed: ${this.speed}');
             }
 
             // (P) Toggle pause
